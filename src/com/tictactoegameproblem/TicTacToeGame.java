f45b3ab07baffd904e3d38cb8e2f023c8df97a7b
sumi <sumipavib1302@gmail.com>
2023-01-31 22:47:16 +0530

    Added:Creating Empty Game Board for game

--- src/com/tictactoegameproblem/TicTacToeGame.java
+++ src/com/tictactoegameproblem/TicTacToeGame.java
@@ -2,4 +2,21 @@ package com.tictactoegameproblem;
 
 public class TicTacToeGame {
 
+	 public static void main(String[] args) {
+	        System.out.println("Welcome in tic tac toy ");
+	       createEmptyBoard();
+	    }
+	    public static void createEmptyBoard(){
+	        char[] sizeOfBoard = new char[10];
+	        System.out.println(" ____________");
+	        for (int i = 1; i <10; i++){
+	            System.out.print("|___");
+	            if (i % 3 == 0) {
+	                System.out.println("|");
+	            }
+
+	        }
+	       
+	    }
+
 }
