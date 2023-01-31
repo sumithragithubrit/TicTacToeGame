5f682b412b6211aab08e836db10204191e4ece02
sumi <sumipavib1302@gmail.com>
2023-01-31 22:56:19 +0530

    Added:Show The Current Board

--- src/com/tictactoegameproblem/TicTacToeGame.java
+++ src/com/tictactoegameproblem/TicTacToeGame.java
@@ -7,9 +7,11 @@ public class TicTacToeGame {
         System.out.println("Welcome in tic tac toy ");
        createEmptyBoard();
        allowPlayer();
+       showBoard();
     }
+	//create empty board
     public static void createEmptyBoard(){
-        char[] sizeOfBoard = new char[10];
+
         System.out.println(" ____________");
         for (int i = 1; i <10; i++){
             System.out.print("|___");
@@ -19,6 +21,7 @@ public class TicTacToeGame {
         }
 
     }
+   // allow choice to enter
     public static void allowPlayer(){
         System.out.println("Enter your choice if wont 'X' then press 1 & if you wont 'O' then press 2");
         Scanner sc = new Scanner(System.in);
@@ -32,5 +35,15 @@ public class TicTacToeGame {
         }
 
     }
-
+    //show current board
+    public static void showBoard(){
+        char[] arr = {48,49,50,51,52,53,54,55,56,57};
+        System.out.println(" ____________");
+        for (int i = 1; i <=9; i++){
+            System.out.print("|_"+arr[i]+"_");
+            if (i % 3 == 0) {
+                System.out.println("|");
+            }
+        }
+    }
 }
