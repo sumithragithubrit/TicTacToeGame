735c5a48a450bce42769a1dfbd50aaf18913b67c
sumi <sumipavib1302@gmail.com>
2023-01-31 22:51:21 +0530

    Added:allow player choice either 0 or x

--- src/com/tictactoegameproblem/TicTacToeGame.java
+++ src/com/tictactoegameproblem/TicTacToeGame.java
@@ -1,22 +1,36 @@
 package com.tictactoegameproblem;
+import java.util.Scanner;
 
 public class TicTacToeGame {
 
-	 public static void main(String[] args) {
-	        System.out.println("Welcome in tic tac toy ");
-	       createEmptyBoard();
-	    }
-	    public static void createEmptyBoard(){
-	        char[] sizeOfBoard = new char[10];
-	        System.out.println(" ____________");
-	        for (int i = 1; i <10; i++){
-	            System.out.print("|___");
-	            if (i % 3 == 0) {
-	                System.out.println("|");
-	            }
+	public static void main(String[] args) {
+        System.out.println("Welcome in tic tac toy ");
+       createEmptyBoard();
+       allowPlayer();
+    }
+    public static void createEmptyBoard(){
+        char[] sizeOfBoard = new char[10];
+        System.out.println(" ____________");
+        for (int i = 1; i <10; i++){
+            System.out.print("|___");
+            if (i % 3 == 0) {
+                System.out.println("|");
+            }
+        }
 
-	        }
-	       
-	    }
+    }
+    public static void allowPlayer(){
+        System.out.println("Enter your choice if wont 'X' then press 1 & if you wont 'O' then press 2");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        if ( choice == 1){
+            System.out.println(" player => x");
+            System.out.println("Computer => O");
+        } else if (choice == 2) {
+            System.out.println(" player => O");
+            System.out.println("Computer => X");
+        }
+
+    }
 
 }
